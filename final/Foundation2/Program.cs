2fcc1dba3fb3730cacaced487b1dec1e55fb96d6
elielino360 <appschoolbic@gmail.com>
2024-04-09 14:10:36 +0100

    final

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,19 +1,19 @@
 
-Address address1 = new Address("15 thomas St", "Idaho", "CA", "United State");
+Address address1 = new("15 thomas St", "Idaho", "CA", "United State");
 
 // Create customer
-Customer customer1 = new Customer("Elijah Jeremiah", address1);
+Customer customer1 = new("Elijah Jeremiah", address1);
 
 
-Product product1 = new Product("Biscuit", 1, 10.99m, 2);
-Product product2 = new Product("Coke", 2, 19.99m, 1);
+Product product1 = new("Biscuit", 1, 10.99m, 2);
+Product product2 = new("Coke", 2, 19.99m, 1);
 
-// Create order
-Order order1 = new Order(customer1);
+
+Order order1 = new(customer1);
 order1.AddProduct(product1);
 order1.AddProduct(product2);
 
-// Display order details
+
 Console.WriteLine("Order Details:");
 Console.WriteLine(order1.GetPackingLabel());
 Console.WriteLine(order1.GetShippingLabel());
@@ -22,21 +22,21 @@ Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
 Console.WriteLine("\n---------------------------\n");
 
 
-Address address2 = new Address("Lekki Garden St", "Ajah", "Lagos", "Nigeria");
+Address address2 = new("Lekki Garden St", "Ajah", "Lagos", "Nigeria");
 
-// Create customer
+
 Customer customer2 = new("Mark Prince", address2);
 
-// Create products
+
 Product product3 = new("Pjamas", 3, 15.50m, 1);
 Product product4 = new("Shirt", 4, 12.75m, 3);
 
-// Create order
-Order order2 = new Order(customer2);
+
+Order order2 = new(customer2);
 order2.AddProduct(product3);
 order2.AddProduct(product4);
 
-// Display order details
+
 Console.WriteLine("Order Details:");
 Console.WriteLine(order2.GetPackingLabel());
 Console.WriteLine(order2.GetShippingLabel());
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,7 +4,7 @@ class Program
     {
         var lectureEvent = new Lecture("Pep Talk", "A lecture on Wole soyinka", DateTime.Now.AddDays(30), "2 PM", new Address("123 Elm St", "Springfield", "Lagos", "Nigeria"), "Dr. Wole Soyinka ", 100);
         var receptionEvent = new Reception("Gallery Opening", "An evening of art and conversation", DateTime.Now.AddDays(45), "7 PM", new Address("456 Main St", "Ajah", "Lagos", "Nigeria"), "art@gallery.com");
-        var outdoorEvent = new OutdoorGathering("Community Picnic", "A day out in the sun with family and friends", DateTime.Now.AddDays(60), "11 AM", new Address("789 Park Ave", "Lekki", "Lagos", "Nigeria"), "Sunny with a chance of showers");
+        var outdoorEvent = new OutdoorGathering("Community Picnic", "A day out in the sun with friends and family", DateTime.Now.AddDays(60), "11 AM", new Address("789 Park Ave", "Lekki", "Lagos", "Nigeria"), "Sunny with a chance of showers");
 
         Console.WriteLine("Event Marketing Material:\n");
         Console.WriteLine(lectureEvent.GetStandardDetails());
